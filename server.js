44b912f796441906fb81f98923239a9eaad69944
Nora1476 <nada70979@gmail.com>
2022-05-27 23:41:08 +0900

    test

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const loogger = require("morgan");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+
+const app = express();
+const _path = path.join(__dirname, "./dist");
+
+app.set("port", (process.env.PORT ??= 12010));
+
+app.use(express.static(_path));
+app.use(loogger("tiny"));
+
+app.get("/", (req, res) => {
+  console.log(__dirname);
+  res.send("<h1>Hello Express</h1>");
+});
+
+const PORT = app.get("port");
+app.listen(PORT, () => {
+  console.log(`real nodemon Server running at http://127.0.0.1:${PORT}/`);
+  console.log(`http://127.0.0.1:${PORT}/join`); //쿼리 추가된것도 요렇게 넣어서 같이 확인가능
+  console.log(`http://127.0.0.1:${PORT}/login`);
+});
