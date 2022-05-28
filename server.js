dbd39940b693c432441f4c51729ddfaacca91a98
Nora1476 <nada70979@gmail.com>
2022-05-29 00:20:52 +0900

    a

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const simple_module = require("./simple_module.js");
-const PORT = 3000;
-
 const express = require("express");
-const loogger = require("morgan");
-const fs = require("fs");
-const bodyParser = require("body-parser");
 const app = express();
+const fs = require("fs");
+
+const bodyParser = require("body-parser");
+const simple_module = require("./simple_module.js");
+const PORT = 12010;
 
 const path = require("path");
 const _path = path.join(__dirname, "./dist");
@@ -17,20 +16,9 @@ app.use(bodyParser.json());
 
 app.get("/", simple_module.index);
 
-app.post("/main", simple_module.main);
+app.post("/main", simple_module.postmain);
 
-// app.post("/finish", (req, res) => {
-//   const {
-//     body: { id, pass1, name, mnum1, mnum2, mnum3, email_1, email_2 },
-//   } = req;
-//   console.log(id, pass1, name, mnum1, mnum2, mnum3, email_1, email_2);
-
-//   fs.readFile("./dist/finish.html", function (error, data) {
-//     //fs 라이브러리 file 읽어오는 모듈
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.end(data);
-//   });
-// });
+app.post("/finish", simple_module.finish);
 
 app.listen(PORT, () => {
   console.log(`real nodemon Server running at http://127.0.0.1:${PORT}/`);
--- simple_module.js
+++ simple_module.js
@@ -7,16 +7,28 @@ module.exports.index = (req, res) => {
   });
 };
 
-module.exports.main = (req, res) => {
+module.exports.postmain = (req, res) => {
+  const {
+    body: { id, pass },
+  } = req;
+  console.log(`사용자id : ${id}, pass : ${pass} `);
+
   fs.readFile("./dist/main.html", function (error, data) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
 };
 
-module.exports.postmain = (req, res) => {
+module.exports.finish = (req, res) => {
   const {
-    body: { id, pass },
+    body: { id, pass1, name, mnum1, mnum2, mnum3 },
   } = req;
-  console.log(id, pass);
+  console.log(
+    `회원가입id : ${id}, password: ${pass1}, name: ${name}, mobile: ${mnum1} ${mnum2} ${mnum3}`
+  );
+
+  fs.readFile("./dist/finish.html", function (error, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 };
