962845c1896ac1e7a7efaf2c591fefc13596bf17
Nora1476 <nada70979@gmail.com>
2022-05-29 21:18:32 +0900

    a

--- server.js
+++ server.js
@@ -14,14 +14,13 @@ app.use(express.static(_path));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", simple_module.index);
-
+app.get("/", simple_module.index);  
+//get 요청이오면 simple_module.index 미들웨어로 처리하겠다. 대신 경로는 "/"이다
 app.post("/main", simple_module.postmain);
-
 app.post("/finish", simple_module.finish);
 
 app.listen(PORT, () => {
   console.log(`real nodemon Server running at http://127.0.0.1:${PORT}/`);
-  // console.log(`http://127.0.0.1:${PORT}/join`); //쿼리 추가된것도 요렇게 넣어서 바로 확인가능
+  // console.log(`http://127.0.0.1:${PORT}/main`); //쿼리 추가해서 요렇게도 바로 확인가능
   // console.log(`http://127.0.0.1:${PORT}/login`);
 });
